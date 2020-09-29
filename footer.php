a5d23460a07f4d89bb45b602af4080f728d45e59
AlanSFSU <ang3@mail.sfsu.edu>
2020-09-28 23:15:18 -0700

    added template and index files

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!--p = padding -->
+<!--my = margin -->
+<!--bg-dark = dark background -->
+
+<div class="container-fluid p-3 my-3 bg-dark text-white footer">
+  <h1>this is the footer</h1>
+
+</div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+  <!-- navigation bar -->
+  <div id="navi">
+      <!-- tabs -->
+      <ul class = "nav nav-tabs" role="tablist">
+          <li class="nav-item">
+              <a class="nav-link active" data-toggle="tab" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#menu1">Menu 1</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+          </li>
+      </ul>
+  </div>
