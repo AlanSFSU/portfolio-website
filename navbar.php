4b1bdad43a69e5990494010cb3060b93bf2ac7fc
AlanSFSU <ang3@mail.sfsu.edu>
2020-10-04 00:18:05 -0700

    finished templating website and implemented smooth scroll

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
   <!-- navigation bar -->
-  <div class ="container">
+  <div class ="container-fluid">
       <!-- tabs -->
       <nav class = "navbar" role="tablist">
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#home">Projects</a></li>
-          <li><a href="#home">Contact me</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
